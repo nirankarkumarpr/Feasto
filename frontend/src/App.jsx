e9a169299621269ff70eace811b3ad74edf736b7
nirankarkumarpr <nirankarkumarpr@gmail.com>
2026-04-15 23:06:13 +0530

    Added cart notification component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import NotFound from "./pages/NotFound";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import CartNotification from "./components/CartNotification";
 
 function App() {
   const location = useLocation();
@@ -62,6 +63,8 @@ function App() {
           </main>
 
           {!hideFooter && <Footer />}
+          
+          <CartNotification />
         </div>
       </CartProvider>
     </AuthProvider>
